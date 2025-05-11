036adcbe367955fc76bc9854afbf26823e57d6db
CorentinPtrl <corentin.pitrel@technofy.io>
2025-05-11 17:02:27 +0200

    feat(docs): ios

--- ios.go
+++ ios.go
@@ -6,6 +6,9 @@ import (
 	"reflect"
 )
 
+// Unmarshal parses the given config into the provided struct pointer.
+// It expects the struct to be a pointer to a struct type.
+// If the provided value is not a pointer to a struct, it will return an error.
 func Unmarshal(data string, v any) error {
 	vValue := reflect.ValueOf(v)
 
@@ -15,6 +18,8 @@ func Unmarshal(data string, v any) error {
 	return internal.Parse(data, v)
 }
 
+// Marshal generates a config string from the provided struct.
+// It can handle both struct and pointer to struct types.
 func Marshal(v any) (string, error) {
 	vValue := reflect.ValueOf(v)
 
@@ -25,6 +30,9 @@ func Marshal(v any) (string, error) {
 	return internal.Generate(vValue.Interface(), vValue.Interface())
 }
 
+// Diff compares two structs of the same type and returns a string representation of the differences.
+// It expects both src and dest to be of the same type.
+// If they are not, it will return an error.
 func Diff(src any, dest any) (string, error) {
 	if src == nil || dest == nil {
 		return "", fmt.Errorf("src or dest is nil")
